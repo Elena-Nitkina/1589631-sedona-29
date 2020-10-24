c158eae88979baa2ff2b6df1d92633549f8d125b
Elena-Nitkina <egrushova_elena@mail.ru>
2020-10-24 12:03:20 +0300

    Оживляем интерактивные элементы

--- js/script.js
+++ js/script.js
@@ -1,24 +1,22 @@
-let myForm = document.querySelector('.search-form');
-let myButton = document.querySelector('.modal-button');
-let searchButton = document.querySelector('.search-form-button');
+let modal = document.querySelector('.search-form');
+let button = document.querySelector('.modal-button');
 let dates = document.querySelectorAll('.date-field');
 let guests = document.querySelectorAll('.number-field');
 
-
 let isStorageSupport = true;
 
-myForm.classList.toggle('modal-show')
-myButton.addEventListener('click', function() {
-    myForm.classList.toggle('modal-hidden');
-    myForm.classList.toggle('modal-show');
+modal.classList.toggle('modal-show')
+button.addEventListener('click', function() {
+    modal.classList.toggle('modal-hidden');
+    modal.classList.toggle('modal-show');
 });
 
-myForm.addEventListener('submit', function(evt) {
+modal.addEventListener('submit', function(evt) {
     if(!dates.item(0).value || !dates.item(1).value || !guests.item(0).value || !guests.item(1).value){
         evt.preventDefault();
-        myForm.classList.remove('modal-error');
-        myForm.offsetWidth = myForm.offsetWidth;
-        myForm.classList.add('modal-error');
+        modal.classList.remove('modal-error');
+        modal.offsetWidth = modal.offsetWidth;
+        modal.classList.add('modal-error');
        
     } else {
         if (isStorageSupport) {
